0960f7f264bce78878c62b8d9e40949ac04b6913
Kahl Awi <167455398+kahlawi456@users.noreply.github.com>
2025-02-11 18:30:16 +0800

    Create Index.js

---
+++ src/pages/index/Index.js
@@ -0,0 +1,16 @@
+/*
+-------------------------------------------------------------------------------------
+Authors         : Alinsonorin, John Myl B., Awi, Joseph Kahl L., Gozon, Daniel Allan
+Date Created    : February 11, 2025
+File            : Index.js
+Stylesheet      : Index.css
+Description     : 
+    This page shows the homepage of the website. This will be the index page as it
+will show the features of the website such as the sign up and login feature, viewing
+of the services, and etc.
+Copyright © 2025. All rights reserved.
+
+Last Modified By: Joseph Kahl L. Awi
+Last Modified On: February 11, 2025
+-------------------------------------------------------------------------------------
+*/
